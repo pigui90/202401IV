c9ef90714a1cf03e0d5997f4f63d03e5c3062777
Sergio <Sergio@LAPTOP-CAAB5F6K>
2024-01-24 16:30:57 -0600

    Semana 13

---
+++ Semana12024/src/semana12024/Semana12024.java
@@ -0,0 +1,22 @@
+package semana12024;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Sergio
+ */
+public class Semana12024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
